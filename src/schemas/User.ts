f336cbf6324fe35cb5257cc7cab47f4d8699d0b6
Leonardo Isso <leoisso.work@gmail.com>
2020-04-01 16:39:17 -0300

    Upload to git

---
+++ src/schemas/User.ts
@@ -0,0 +1,17 @@
+import { Document, Schema, model } from 'mongoose'
+
+interface UserInterface extends Document {
+  email?: string
+  firstName?: string
+  lastName?: string
+}
+
+const UserSchema = new Schema({
+  email: String,
+  firstName: String,
+  lastName: String
+}, {
+  timestamps: true
+})
+
+export default model<UserInterface>('User', UserSchema)
